2e28e00f5342513bd4e0b0ff8ffa1b6ffbe57252
Stub Roh <stubroh@MacBookAir.local>
2021-10-07 19:33:09 +0900

    Add new reference didDocument

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,8 @@
 import request from 'supertest';
 import { createServer } from 'http';
 import faker from 'faker/locale/ko';
-import { createApp } from '../src/create-app';
-import { config } from '../src/config';
+// import { createApp } from '../src/create-app';
+// import { config } from '../src/config';
 
 let server;
 
@@ -10,17 +10,17 @@ beforeAll(async () => {
   // TODO DB initialize - TX start
 
   // DO Web Server initalize
-  const app = createApp();
-  const port = process.env.APP_PORT;
-  server = await createServer(app).listen(port, () => {
-    console.log(`${process.env.APP_NAME} Listening on port ${port}`);
-  });
-  global.agent = request.agent(server);
+  // const app = createApp();
+  // const port = process.env.APP_PORT;
+  // server = await createServer(app).listen(port, () => {
+  //   console.log(`${process.env.APP_NAME} Listening on port ${port}`);
+  // });
+  // global.agent = request.agent(server);
 });
 
 afterAll(async () => {
-  // DO Web Server close
-  await server.close();
-  // TODO DB close - TX rollback
-  await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
+  // // DO Web Server close
+  // await server.close();
+  // // TODO DB close - TX rollback
+  // await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
 });
