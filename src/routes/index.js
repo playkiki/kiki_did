14239a0aa5ed3eac4fbd909d9929002861bec886
stub <stub@gamebin.co.kr>
2021-11-26 16:15:49 +0900

    Refactor - pubclaim api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { didRouter } from './did';
 import { docclaimRouter } from './docclaim';
 import { reqclaimRouter } from './reqclaim';
+import { pubclaimRouter } from './pubclaim';
 
 export const rootRouter = express.Router();
-rootRouter.use('/api/v1/kiki', didRouter);
 rootRouter.use('/api/v1/kiki', docclaimRouter);
 rootRouter.use('/api/v1/kiki', reqclaimRouter);
+rootRouter.use('/api/v1/kiki', pubclaimRouter);
