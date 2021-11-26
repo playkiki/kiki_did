bd8bc6fa53f985a9324b58121581d5b7e730f72a
stub <stub@gamebin.co.kr>
2021-11-26 16:12:21 +0900

    Refactor - reqclaim api

--- src/routes/did.js
+++ src/routes/did.js
@@ -2,70 +2,8 @@ import crypto from 'crypto-js';
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 import { check, validationResult } from 'express-validator/check';
-import { Utils } from '../utils';
 
 export const didRouter = express.Router();
-
-didRouter.post(
-  '/reqclaim',
-  [
-    check('userPri')
-      .not()
-      .isEmpty()
-      .withMessage('userPri must required'),
-    check('didID')
-      .not()
-      .isEmpty()
-      .withMessage('didID must required')
-  ],
-  asyncHandler(async (req, res) => {
-    const errors = validationResult(req);
-
-    let rtnBody = {};
-    rtnBody.success = false;
-    rtnBody.errorcode = '';
-    rtnBody.errordetail = '';
-
-    if (!errors.isEmpty()) {
-      rtnBody.errorcode = 'KDE0001';
-      rtnBody.errordetail = 'Req. params not existed';
-      return res.status(422).jsonp(rtnBody);
-    } else {
-      console.log('--reqclaim--', ' req.body:', req.body);
-
-      let { userPri, didID } = req.body;
-
-      // TODO. verify user public / private process
-
-      // Query didID and return encrypted claimDoc
-      const sequelize = Utils.createConnection();
-      const ClaimDocs = sequelize.import('../models/claim_docs');
-    
-      ClaimDocs.findOne({
-        where : {
-          didID : didID
-        },
-        raw : true
-      })
-        .then(claimDocRtn => {
-          console.log('success to find claim doc : ', claimDocRtn.claims);
-          // return res.status(200).send(JSON.stringify(claimDocRtn.claims));
-          rtnBody.success = true;
-          rtnBody.result = claimDocRtn.claims;
-          return res.status(200).jsonp(rtnBody);
-        })
-        .catch(err => {
-          console.error('fail to find claim doc : ', err);
-          rtnBody.errorcode = 'KDE0010';
-          rtnBody.errordetail = err;
-          return res.status(404).jsonp(rtnBody);
-        })
-        .finally(() => {
-          sequelize.close();
-        })
-    }
-  }) 
-);
 
 didRouter.post(
   '/pubclaim',
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { didRouter } from './did';
 import { docclaimRouter } from './docclaim';
+import { reqclaimRouter } from './reqclaim';
 
 export const rootRouter = express.Router();
 rootRouter.use('/api/v1/kiki', didRouter);
 rootRouter.use('/api/v1/kiki', docclaimRouter);
+rootRouter.use('/api/v1/kiki', reqclaimRouter);
